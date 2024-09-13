4cfc02a2b0cadc8188d53a0eb9c4550c6d81b653
olawalesegun <olawalesegun0@gmail.com>
2024-09-13 18:11:50 +0300

    mainnet forking with uniswap add liquidity

--- scripts/mainnet-forking.ts
+++ scripts/mainnet-forking.ts
@@ -1,49 +1,72 @@
 import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
-async function main() {
-    const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-    const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-    const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const main = async () => {
+  const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  const UniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+  const TokenHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
 
-    const TOKEN_HOLDER = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
+  await helpers.impersonateAccount(TokenHolder);
+  const impersonatedSigner = await ethers.getSigner(TokenHolder);
 
-    await helpers.impersonateAccount(TOKEN_HOLDER);
-    const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
+  const amountUSDC = ethers.parseUnits("1000", 6);
+  const amountDAI = ethers.parseUnits("1000", 18);
 
-    const amountOut = ethers.parseUnits("20", 18);
-    const amountInMax = ethers.parseUnits("1000", 6);
+  const amountUSDCMin = ethers.parseUnits("950", 6);
+  const amountDAIMin = ethers.parseUnits("950", 18);
 
-    const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
-    const DAI_Contract = await ethers.getContractAt("IERC20", DAI);
-    
-    const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
+  const deadline = Math.floor(Date.now() / 1000) + 60 * 10;
 
-    await USDC_Contract.approve(ROUTER, amountOut);
+  const USDC = await ethers.getContractAt(
+    "IERC20",
+    USDCAddress,
+    impersonatedSigner
+  );
 
-    const usdcBal = await USDC_Contract.balanceOf(impersonatedSigner.address);
-    const daiBal = await DAI_Contract.balanceOf(impersonatedSigner.address);
-    const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
+  const DAI = await ethers.getContractAt(
+    "IERC20",
+    DAIAddress,
+    impersonatedSigner
+  );
 
-    console.log("usdc balance before swap", Number(usdcBal));
-    console.log("dai balance before swap", Number(daiBal));
+  const ROUTER = await ethers.getContractAt(
+    "IUniswapV2Router",
+    UniswapRouter,
+    impersonatedSigner
+  );
 
-    await ROUTER.swapTokensForExactTokens(
-        amountOut,
-        amountInMax,
-        [USDC, DAI],
-        impersonatedSigner.address,
-        deadline
-    );
+  await USDC.approve(UniswapRouter, amountUSDC);
+  await DAI.approve(UniswapRouter, amountDAI);
 
-    const usdcBalAfter = await USDC_Contract.balanceOf(impersonatedSigner.address);
-    const daiBalAfter = await DAI_Contract.balanceOf(impersonatedSigner.address);
+  const usdcBalBefore = await USDC.balanceOf(impersonatedSigner.address);
+  const daiBalBefore = await DAI.balanceOf(impersonatedSigner.address);
 
-    console.log("=========================================================");
+  console.log(
+    "Balance before adding liquidity:::",
+    Number(usdcBalBefore),
+    Number(daiBalBefore)
+  );
 
-    console.log("usdc balance after swap", Number(usdcBalAfter));
-    console.log("dai balance after swap", Number(daiBalAfter));
-}
+  const tx = await ROUTER.addLiquidity(
+    USDCAddress,
+    DAIAddress,
+    amountUSDC,
+    amountDAI,
+    amountUSDCMin,
+    amountDAIMin,
+    impersonatedSigner.address,
+    deadline
+  );
+  await tx.wait();
+
+  const usdcBalAfter = await USDC.balanceOf(impersonatedSigner.address);
+  const daiBalAfter = await DAI.balanceOf(impersonatedSigner.address);
+
+  console.log("USDC Balance after adding liquidity:", Number(usdcBalAfter));
+  console.log("DAI Balance after adding liquidity:", Number(daiBalAfter));
+
+};
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
